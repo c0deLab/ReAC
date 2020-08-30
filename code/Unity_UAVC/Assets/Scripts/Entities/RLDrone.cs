4b3d19a2d13cb06b161e7c751100377b11dc3e93
SakuraiSatoru <fangzhihao126@gmail.com>
2020-08-30 23:25:01 +0800

    RLDrone initialization

--- code/Unity_UAVC/Assets/Scripts/Entities/RLDrone.cs
+++ code/Unity_UAVC/Assets/Scripts/Entities/RLDrone.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Sensors.Reflection;
 using UnityEngine.Assertions;
 
 public class RLDrone : Agent
@@ -12,11 +14,19 @@ public class RLDrone : Agent
     private Vector3 _lastGoToPos; // used to compare and reset PIDController
     private Vector3 _lastGoAtVel; // used to compare and reset PIDController
     private Transform _naviTarget;
-    private Transform dock;
+    private Transform _target;
+    private SphereCollider _collider;
+    private RLConfig _envConfig;
 
-    private float _posTolerance = 0.01f;
     private float _tiltSmoothTime = 1.0f;
     private Vector2 _tiltVelocity;
+    
+    public float ColliderRangeMin = 0.25f;
+    public float ColliderRangeMax = 0.40f;
+
+    
+    [Observable]
+    public float ColliderRadius => _collider.radius;
 
     private void Awake()
     {
@@ -24,16 +34,59 @@ public class RLDrone : Agent
         _controller = new PIDController(_rigidbody, 1.5f, 0.0f, 0.1f);
         _naviTarget = new GameObject("NaviTarget").transform;
         _naviTarget.parent = transform;
-        dock = new GameObject($"Dock {name}").transform;
-        dock.position = transform.position;
-        dock.rotation = transform.rotation;
+        _target = new GameObject("Target").transform;
+        _target.parent = transform;
+        _collider = GetComponent<SphereCollider>();
+        _envConfig = transform.parent.GetComponent<RLConfig>();
     }
+
+    private bool IsCollided(Collider collider)
+    {
+        foreach (Transform child in transform.parent)
+        {
+            if (child == transform) continue;
+            if (collider.bounds.Intersects(child.GetComponent<Collider>().bounds)) return true;
+            var childTarget = child.Find("Target");
+            if (childTarget != null && Vector3.Distance(collider.transform.position, childTarget.position) <=
+                ColliderRangeMax * 2) return true;
+        }
+        
+        return false;
+    }
+
 
     public override void OnEpisodeBegin()
     {
-        transform.position = dock.position;
-        transform.rotation = dock.rotation;
+        var respawnRange = _envConfig.RespawnDistance;
+        var targetDist = _envConfig.TargetDistance;
         _rigidbody.velocity = new Vector3();
+        _collider.radius = Random.Range(ColliderRangeMin, ColliderRangeMax);
+
+        bool CheckInit() => Vector3.Distance(transform.position, Vector3.zero) <= _envConfig.RespawnDistance && !IsCollided(_collider);
+        do
+        {
+            transform.position = new Vector3(Random.Range(-respawnRange, respawnRange), 0, Random.Range(-respawnRange, respawnRange));
+            Physics.SyncTransforms();
+        } while (!CheckInit());
+
+        var targetCollider = _target.gameObject.AddComponent<SphereCollider>();
+        targetCollider.radius = ColliderRangeMax;
+        targetCollider.isTrigger = true;
+
+        bool CheckTargetInit()
+        {
+            var distOrigin = Vector3.Distance(Vector3.zero, _target.position);
+            var distDrone = Vector3.Distance(transform.position, _target.position);
+            return (distOrigin <= respawnRange) && (distDrone > targetDist - 1) && (distDrone < targetDist + 1) && !IsCollided(targetCollider);
+        }
+
+        do
+        {
+            _target.position = new Vector3(Random.Range(-respawnRange, respawnRange), 0, Random.Range(-respawnRange, respawnRange));
+            Physics.SyncTransforms();
+        } while (!CheckTargetInit());
+        DestroyImmediate(targetCollider);
+
     }
 
     public override void CollectObservations(VectorSensor sensor)
@@ -44,11 +97,9 @@ public class RLDrone : Agent
 
     public override void OnActionReceived(float[] vectorAction)
     {
-        // _rigidbody.velocity = new Vector3(vectorAction[0], vectorAction[1], vectorAction[2]);
         GoAtVel(new Vector3(vectorAction[0], vectorAction[1], vectorAction[2]));
     }
 
-    // Update is called once per frame
     private void FixedUpdate()
     {
         HandleTilt();
@@ -77,33 +128,16 @@ public class RLDrone : Agent
         _controller.GoAtVel(targetVel);
     }
 
-    public void Stop()
-    {
-        _rigidbody.velocity = new Vector3();
-    }
-
     private void HandleTilt()
     {
-        float pitch = -_rigidbody.velocity.x * 10;
-        float roll = _rigidbody.velocity.z * 10;
+        var pitch = -_rigidbody.velocity.x * 10;
+        var roll = _rigidbody.velocity.z * 10;
 
         _tiltVelocity.x = Mathf.Lerp(_tiltVelocity.x, pitch, Time.deltaTime * 10);
         _tiltVelocity.y = Mathf.Lerp(_tiltVelocity.y, roll, Time.deltaTime * 10);
 
-        // Quaternion rot = Quaternion.Euler(0, 0, tiltVelocity.x);
-        Quaternion rot = Quaternion.Euler(_tiltVelocity.y, 0, _tiltVelocity.x);
+        var rot = Quaternion.Euler(_tiltVelocity.y, 0, _tiltVelocity.x);
         _rigidbody.MoveRotation(rot);
     }
 
-    private bool ReachedNaviTargetPos()
-    {
-        return Vector3.Distance(transform.position, _naviTarget.position) <= _posTolerance;
-    }
-
-    public void AddRandomForce()
-    {
-        float f = 1000.0f;
-        _rigidbody.AddForce(new Vector3(Random.Range(-f, f), Random.Range(-f, f), Random.Range(-f, f)),
-            ForceMode.Acceleration);
-    }
 }
---
+++ code/Unity_UAVC/Assets/Scripts/RLConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RLConfig : MonoBehaviour
+{
+    public float RespawnDistance = 7.0f;
+    public float TargetDistance = 7.0f;
+}
